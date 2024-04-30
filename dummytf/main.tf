d622346eaa7bf5fd149239a73bd956ab55a27bd4
skeptic-aptitude <137830713+skeptic-aptitude@users.noreply.github.com>
2024-04-30 20:19:26 +0200

    testing azure login with OIDC

---
+++ dummytf/main.tf
@@ -0,0 +1,10 @@
+resource "null_resource" "example" {
+  triggers = {
+    // This will trigger the resource to be recreated on every apply
+    timestamp = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'Hello, World!'"
+  }
+}
