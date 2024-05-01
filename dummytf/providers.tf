e0092c9950bcc820d7eaddc33435d5bd1ee5878a
skeptic-aptitude <137830713+skeptic-aptitude@users.noreply.github.com>
2024-05-01 17:03:00 +0200

    testing OIDC in terraform and digger workflow

--- dummytf/providers.tf
+++ dummytf/providers.tf
@@ -1,8 +1,32 @@
 terraform {
   required_providers {
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.101.0"
+    }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0"
+      version = "~> 3.2.2"
     }
+
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-mrjn-sandbox"
+    storage_account_name = "mrjndevopstfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+    use_azuread_auth     = true
+    subscription_id      = var.azure_subscription_id
+    tenant_id            = var.azure_tenant_id
+    client_id            = var.azure_client_id
   }
 }
+
+provider "azurerm" {
+  features {}
+}
+
+provider "null" {}
---
+++ dummytf/variables.tf
@@ -0,0 +1,17 @@
+variable "azure_client_id" {
+  description = "Azure Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  sensitive   = true
+}
