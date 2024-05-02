fe2591034c2ee6198f9e731002685f7b1a39aa67
skeptic-aptitude <137830713+skeptic-aptitude@users.noreply.github.com>
2024-05-02 10:32:12 +0200

    removed incorrect variable declaration from providers

--- dummytf/providers.tf
+++ dummytf/providers.tf
@@ -19,9 +19,6 @@ terraform {
     key                  = "terraform.tfstate"
     use_oidc             = true
     use_azuread_auth     = true
-    subscription_id      = var.azure_subscription_id
-    tenant_id            = var.azure_tenant_id
-    client_id            = var.azure_client_id
   }
 }
 
